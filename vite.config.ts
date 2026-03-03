6e683f402f1ced924661268adbd37dda7d4744dd
SeungMin Park <psm1757@gmail.com>
2026-03-04 05:39:46 +0900

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+    base: '/game-for-son-4/',
     plugins: [
         VitePWA({
             registerType: 'autoUpdate',
